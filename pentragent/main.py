ed91f4f02ae501d5d23904c33a6da4da2e2debd1
William <wjsobral1000@gmail..com>
2025-04-06 16:38:56 -0400

    Example Request

--- pentragent/main.py
+++ pentragent/main.py
@@ -6,38 +6,44 @@
 # TODO: Add hault condition
 # Compile all command notes to summarize the effects of the attacks and the security of the system
 
-from docker import client
 from pydantic import BaseModel, Field
 from datetime import date
 from enum import Enum
 from typing import List
+from openai import OpenAI
 
+# Making the request
+client = OpenAI(
+    api_key="",
+    base_url="https://api.x.ai/v1/",
+)
 
 def main():
-    make_initial_call()
-    send_bash_command()
-    make_subsequent_calls()
-    compile_notes()
-    save_json()
+    response = make_initial_call()
+    print(type(response)
+    #send_bash_command(response)
+    #make_subsequent_calls()
+    #compile_notes()
+    #save_json()
 
+initial_system_prompt = """
+Generate bash commands to execute. 
+"""
 
 def make_initial_call():
-    completion = client.Completion.create(
-        model="gpt-3.5-turbo",
+    completion = client.beta.chat.completions.parse(
+        model="grok-2-latest",
         messages=[
-            {"role": "user", "content": "What is the current date?"},
-            {"role": "user", "content": "What is the current date?"},
+            {"role": "system", "content": initial_system_prompt},
+            {"role": "user", "content": "make a directory"},
         ], 
         response_format=FormattedResponse
     )
-    response = FormattedResponse(**completion.choices[0].message.content)
-    return response
+    return completion.choices[0].message.parsed
 
 def send_bash_command():
     pass
 def add_more_context_to_system_prompt():
-    pass
-def generate_gameplan():
     pass
 def make_subsequent_calls():
     pass    
@@ -52,5 +58,7 @@ class FormattedResponse(BaseModel):
     topic: str = Field(description="Topic of the bash command")
     log: str = Field(description="Log entry for command context")
     success: bool = Field(description="Success of the command")
+
+
 if __name__ == "__main__":
     main()
